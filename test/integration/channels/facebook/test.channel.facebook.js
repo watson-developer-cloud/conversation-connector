b9458d4b9794978ea008e24534a905bda4842d01
David <dgterry@us.ibm.com>
2018-01-24 13:00:32 -0500

    sha

--- test/integration/channels/facebook/test.channel.facebook.js
+++ test/integration/channels/facebook/test.channel.facebook.js
@@ -36,7 +36,7 @@ const shaMap = {
   'hello, world!': 'sha1=e7d42cb171ec3ca26c2d6de14635bd3ea04bf01d',
   hi: 'sha1=3bcbbbd11ad8ef728dba5d9d903e55abdea24738',
   hogwarts: 'sha1=eb4412b17e32da9656bb3e3551094d531438b6da',
-  multi: 'sha1=74e09fb67c7792a0bdc71369e4309b17c7b1e4d0'
+  multi: 'sha1=d13e82a9fe82a228a40e3bb1a025261cc8489c24'
 };
 
 /** Function allows tests to sleep for certain amount of time
